75a8e9136fd0ef43ad8b1be85160f7bc456c5356
Matheus Carvalho <matheus10277@gmail.com>
2021-08-09 11:21:01 -0300

    Creating active list item component

---
+++ src/components/ShoppingListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="item">
+    <p class="item-name">{{ item.name }}</p>
+    <slot />
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+import Item from "@/models/Item.interface";
+
+export default defineComponent({
+  name: "ShoppingListItem",
+  props: {
+    item: {
+      type: Object as PropType<Item>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &-name {
+    font-size: 1.4rem;
+  }
+}
+</style>
