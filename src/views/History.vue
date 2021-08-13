9f1aaea1470a1026009399330fcf675d576546f6
Matheus Carvalho <matheus10277@gmail.com>
2021-08-13 19:22:49 -0300

    Implementing 'add new' logic in history view

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="history">
-    <h1>Shopping history</h1>
+    <div class="history__header">
+      <h1>Shopping history</h1>
+      <button class="add" @click="showForm = true">
+        Add new <span class="material-icons"> add </span>
+      </button>
+    </div>
     <ul class="lists">
       <Shopping-list :list="list" v-for="list in lists" :key="list.id" />
     </ul>
+
+    <Mask v-show="showForm">
+      <Add-List-Form @close="showForm = false"/>
+    </Mask>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import Mask from "@/components/Mask.vue";
+import AddListForm from "@/components/AddListForm.vue";
 import ShoppingList from "@/components/ShoppingList.vue";
 
 export default defineComponent({
   name: "History",
-  components: { ShoppingList },
+  components: { ShoppingList, Mask, AddListForm },
+  data() {
+    return {
+      showForm: false,
+    };
+  },
   computed: {
     lists() {
       return this.$store.state.lists;
@@ -27,11 +43,27 @@ export default defineComponent({
 .history {
   padding: 3rem 2rem;
 
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 3rem;
+  }
+
   h1 {
     color: #34333a;
     font-size: 2.6rem;
     line-height: 3.2rem;
-    margin-bottom: 3rem;
+  }
+
+  .add {
+    display: flex;
+    column-gap: 0.3rem;
+    align-items: center;
+
+    background: 0;
+    color: #56ccf2;
   }
 }
 
