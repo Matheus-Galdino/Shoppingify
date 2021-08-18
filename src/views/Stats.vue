fd901a9416a8ad2752c66c164d192ce139bc909d
Matheus Carvalho <matheus10277@gmail.com>
2021-08-18 16:01:59 -0300

    Adding apexchart with monthly summary data

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,28 +1,35 @@
 <template>
   <div class="stats">
-    <h2>Top items</h2>
-    <ul class="top-items">
-      <li v-for="(item, index) in topItems" :key="index">
-        <p>
-          <span> {{ item.key }} </span>
-          <span class="percentage"> {{ item.percentage }}% </span>
-        </p>
+    <section class="top-container">
+      <h2>Top items</h2>
+      <ul class="top-items">
+        <li v-for="(item, index) in topItems" :key="index">
+          <p>
+            <span> {{ item.key }} </span>
+            <span class="percentage"> {{ item.percentage }}% </span>
+          </p>
 
-        <Progress :percentage="item.percentage" background="#F9A109" />
-      </li>
-    </ul>
+          <Progress :percentage="item.percentage" background="#F9A109" />
+        </li>
+      </ul>
 
-    <h2>Top categories</h2>
-    <ul class="top-categories">
-      <li v-for="(category, index) in topCategories" :key="index">
-        <p>
-          <span> {{ category.key }} </span>
-          <span> {{ category.percentage }}% </span>
-        </p>
+      <h2>Top categories</h2>
+      <ul class="top-categories">
+        <li v-for="(category, index) in topCategories" :key="index">
+          <p>
+            <span> {{ category.key }} </span>
+            <span> {{ category.percentage }}% </span>
+          </p>
 
-        <Progress :percentage="category.percentage" background="#56CCF2" />
-      </li>
-    </ul>
+          <Progress :percentage="category.percentage" background="#56CCF2" />
+        </li>
+      </ul>
+    </section>
+
+    <section class="monthly-container">
+      <h2>Monthly summary</h2>
+      <apexchart width="100%" :options="options" :series="series"></apexchart>
+    </section>
   </div>
 </template>
 
@@ -39,6 +46,49 @@ export default defineComponent({
     },
     topCategories() {
       return this.$store.state.topCategories;
+    },
+    options() {
+      return {
+        chart: {
+          id: "monthly-summary",
+          fontFamily: "Quicksand, sans-serif",
+          toolbar: {
+            show: false,
+          },
+        },
+        grid: {
+          borderColor: "#E0E0E0",
+          strokeDashArray: 5,
+          xaxis: {
+            lines: {
+              show: true,
+            },
+          },
+        },
+        markers: {
+          size: 3,
+          colors: ["#fff"],
+          strokeColors: ["#F9A109"],
+        },
+        stroke: {
+          width: 1,
+          curve: "smooth",
+          colors: ["#F9A109"],
+        },
+        xaxis: {
+          categories: this.$store.state.monthlySummary.map((summ) => summ.key),
+        },
+      };
+    },
+    series() {
+      const data = this.$store.state.monthlySummary.map((summ) => summ.amount);
+
+      return [
+        {
+          name: "Months",
+          data,
+        },
+      ];
     },
   },
 });
