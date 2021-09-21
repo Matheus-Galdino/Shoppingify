e52cd59bb2a90d74da4d034e8eed5d08acbb1002
Matheus Carvalho <matheus10277@gmail.com>
2021-09-21 14:21:15 -0300

    Creating auth form components

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <form @submit.prevent="signIn">
+    <h1 class="title">Sign In</h1>
+
+    <CustomInput
+      id="email"
+      type="email"
+      label="Email"
+      v-model="user.email"
+      placeholder="email@example.com"
+    />
+
+    <CustomInput
+      id="password"
+      type="password"
+      label="Password"
+      v-model="user.password"
+      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+    />
+
+    <CustomCheckbox
+      id="keepConnected"
+      label="Keep connected"
+      v-model="keepConnected"
+    />
+
+    <div class="action-group">
+      <button>Sign in</button>
+      <router-link to="/auth">Sign up</router-link>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import API from "@/API";
+import User from "@/models/User.interface";
+
+import CustomInput from "@/components/CustomInput.vue";
+import CustomCheckbox from "@/components/CustomCheckbox.vue";
+
+export default defineComponent({
+  name: "LoginForm",
+  components: { CustomInput, CustomCheckbox },
+  data() {
+    return {
+      user: {} as User,
+      keepConnected: false,
+    };
+  },
+  methods: {
+    async signIn() {
+      const token = await API.Signin(this.user);
+      console.log(token);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.checkbox-container {
+  margin-top: 1rem;
+}
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <form @submit.prevent="signup">
+    <h1 class="title">Create Account</h1>
+
+    <CustomInput id="name" label="Name" placeholder="Name" v-model="name" />
+
+    <CustomInput
+      id="email"
+      type="email"
+      label="Email"
+      v-model="email"
+      placeholder="email@example.com"
+    />
+
+    <CustomInput
+      id="password"
+      type="password"
+      label="Password"
+      v-model="password"
+      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+    />
+
+    <CustomInput
+      type="password"
+      id="confirmPassword"
+      label="Confirm password"
+      v-model="confirmPassword"
+      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+    />
+
+    <div class="action-group">
+      <button>Sign up</button>
+      <router-link to="/auth/login">Sign in</router-link>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import CustomInput from "@/components/CustomInput.vue";
+
+export default defineComponent({
+  name: "Signup",
+  components: { CustomInput },
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  },
+  methods: {
+    signup() {
+      console.table({ name: this.name, email: this.email, pwd: this.password });
+    },
+  },
+});
+</script>
+
+<style>
+</style>
